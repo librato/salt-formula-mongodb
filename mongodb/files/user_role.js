8321d6b53a669a2172a008727d7841283ce17d47
Farzad Battiwalla <fbattiwalla@librato.com>
2018-06-28 15:50:28 -0400

    Rendered role into json

--- mongodb/files/user_role.js
+++ mongodb/files/user_role.js
@@ -3,6 +3,6 @@
 {%- if database_defs.get('enabled', False) %}
   db = db.getSiblingDB("{{ database_name }}");
   {%- for user_info in database_defs.users %}
-    db.addUser({ user: "{{ user_info['name'] }}", pwd: "{{ user_info['password'] }}", roles: {{ user_info['roles'] }} });
+    db.addUser({ user: "{{ user_info['name'] }}", pwd: "{{ user_info['password'] }}", roles: {{ user_info['roles'] | json }} });
   {%- endfor %}
 {%- endif %}
