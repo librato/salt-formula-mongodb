30496cc9fbd7744e003ef6c2b468bfdf59a86c50
Farzad Battiwalla <fbattiwalla@librato.com>
2018-06-28 15:42:47 -0400

    Fixed Jinja bug

--- mongodb/files/user_role.js
+++ mongodb/files/user_role.js
@@ -2,7 +2,7 @@
 
 {%- if database.get('enabled', False) %}
   db = db.getSiblingDB("{{ database_name }}");
-  {%- for user_info in database.users %}
+  {%- for user_info in database_defs.users %}
     db.addUser({ user: "{{ user_info['name'] }}", pwd: "{{ user_info['password'] }}", roles: {{ user_info['roles'] }} });
   {%- endfor %}
 {%- endif %}
