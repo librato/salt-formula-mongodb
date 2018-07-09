07e58606dfc84224cccf3d0de25a56ef2078794d
Farzad Battiwalla <fbattiwalla@librato.com>
2018-07-09 13:40:32 -0400

    Modified to use db.createUser

--- mongodb/files/user_role.js
+++ mongodb/files/user_role.js
@@ -1,8 +1,10 @@
 {%- from "mongodb/map.jinja" import server with context -%}
 
 {%- if database_defs.get('enabled', False) %}
-  db = db.getSiblingDB("{{ database_name }}");
-  {%- for user_info in database_defs.users %}
-    db.addUser({ user: "{{ user_info['name'] }}", pwd: "{{ user_info['password'] }}", roles: {{ user_info['roles'] | json }} });
-  {%- endfor %}
+
+dbh = db.getSiblingDB("{{ database_name }}");
+{%- for user_info in database_defs.users %}
+dbh.createUser({ user: "{{ user_info['name'] }}", pwd: "{{ user_info['password'] }}", roles: {{ user_info['roles'] | json }} });
+{%- endfor %}
+
 {%- endif %}
