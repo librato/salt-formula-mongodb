83f8747fb1eb32f34d255d47061d9d26b8e485b7
Farzad Battiwalla <fbattiwalla@librato.com>
2018-07-12 12:35:14 -0400

    Removing unused file

--- mongodb/files/admin_user.js
+++
@@ -1,3 +0,0 @@
-{%- from "mongodb/map.jinja" import server with context -%}
-db.addUser("{{ server.admin.user }}","{{ server.admin.password }}")
-db.auth("{{ server.admin.user }}","{{ server.admin.password }}")
